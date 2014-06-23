1eb66eb4428c9a49ebbbc8e2d587080188543d89
Stefan Lau <github@stefanlau.com>
2014-06-23 12:45:03 +0200

    Add first vulnerability: Unchecked redirectTo query parameter for login

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express'),
     exphbs  = require('express3-handlebars'),
+    bodyParser = require('body-parser'),
+    cookieSession = require('cookie-session'),
     app = express();
 
 app.engine('hbs', exphbs({
@@ -8,8 +10,34 @@ app.engine('hbs', exphbs({
 }));
 app.set('view engine', 'hbs');
 
-app.get('/', function(req, res){
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cookieSession({
+    signed: false
+}));
+
+app.get('/', function(req, res) {
     res.render('index');
+});
+
+app.get('/login', function(req, res) {
+    res.render('login', {
+        redirectTo: req.query.redirectTo
+    });
+});
+
+app.post('/login', function(req, res) {
+    if (req.body.username == 'admin' && req.body.password === 'password') {
+        req.session.loggedInAs = 'admin';
+        req.session.loggedIn = true;
+        res.redirect(req.body.redirectTo);
+    } else {
+        res.render('login', {
+            errorMessage: 'Login Failed',
+            redirectTo: req.body.redirectTo
+        });
+    }
 });
 
 app.listen(3000);
