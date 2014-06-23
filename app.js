ad9928a397c8c0b736a78ed32298e736b39609ba
Stefan Lau <github@stefanlau.com>
2014-06-23 12:13:48 +0200

    add express-handlebars template engine

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express'),
+    exphbs  = require('express3-handlebars'),
     app = express();
 
+app.engine('hbs', exphbs({
+    extname: '.hbs',
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'hbs');
+
 app.get('/', function(req, res){
-    res.send('Hello, World!');
+    res.render('index');
 });
 
 app.listen(3000);
